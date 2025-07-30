e1631996c2fbc69841ddbf750e7269a98f07b452
Tuna Ekici <tnaekc2@gmail.com>
2025-07-30 20:06:19 +0300

    -added local telemetry logging

---
+++ rpi_code/main.py
@@ -0,0 +1,409 @@
+from pymavlink import mavutil
+import time, select, socket, numpy as np, sys
+from threading import Thread
+from com.mav import MavConnect
+from com.imgm import RecvClass, SendClass, DetectClass, VideoSave
+
+from sim.simulation import RocketSimulation
+from sim.rocket import RocketModel
+from sim.envr import EnvironmentModel
+
+from util.log import Log
+from util.telem_log import TelemLog
+
+import RPi.GPIO as GPIO
+
+# DELAY CONST
+
+DET_WAIT = 0.1
+MAIN_WAIT = 0.1
+SEND_WAIT = 0.1
+IMG_WAIT = 0
+RECV_WAIT = 0
+LOG_WAIT = 1
+
+ERROR_WAIT = 0.1
+
+FAILSAFE_DELAY = 1.5
+
+
+# NETWORK CONST
+
+IP = "192.168.0.101"
+MSIP = "10.58.7.165"
+PORTS = (14550, 14551, 31313) # send recv mp
+
+
+# SERVO CONST #
+
+MIN_PWM = 2.478
+NET_PWM = 7.11
+MAX_PWM = 12.874
+MDELAY = 1
+SERVO_PIN = 12
+
+SIGNAL_TRESHOLD = 10
+
+
+#############################
+
+img_det = DetectClass("model.pt", 1071, 1071, 320, 240)
+img_recv = RecvClass()
+img_send = SendClass(IP, 5000)
+
+#m/s^2, kg/m^3 -> by meter
+envr = EnvironmentModel(grav = 9.81, ro_path = "", wind_path = "") 
+#kg, kg.m^2, m, m^2, coef of drag -> by velocity
+rocket = RocketModel(mass = 241, carea = 0.06, cd_path = "") 
+sim = RocketSimulation(dt = 0.1, rocket = rocket, envr = envr)
+
+
+#video = VideoSave()
+telem_logger = TelemLog("telem_log.txt")
+
+
+#############################
+
+telemetry_data = {}
+gcs_data = {}
+box_data = []
+
+img_feed = None
+firing = False
+is_det = False
+
+loggr = Log()
+
+loggr.print("Starting Pixhawk Connection...", 3)
+pixhawk = mavutil.mavlink_connection('/dev/ttyAMA0', baud=57600)
+loggr.print("Success!\n", 1)
+
+loggr.print("Starting GCS Connection...", 3)
+mav_com = MavConnect(pixhawk)
+loggr.print("Success!\n", 1)
+
+loggr.print("Starting GPIO...", 3)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+p = GPIO.PWM(SERVO_PIN, 50)
+loggr.print("Success!\n", 1)
+
+read_check = [0, 0, 0, 0]
+signal_lost = False
+
+## APPLY OBJECT DETECTION AND RUN SIMULATION
+def detect_and_fire():
+
+    global box_data, telemetry_data, firing
+
+    while True:
+        try:
+            if (is_det and (img_feed) is not None):
+                raw_box_data = img_det.get_boxes(img_feed)
+                box_data = [[int(box.cls[0].item())] + list(map(int, box.xyxy[0])) for box in raw_box_data] 
+
+                for box in box_data:
+
+                    # RUN SIMULATION
+                    ned = telemetry_data.get("LOCAL_POSITION_NED")
+                    hud = telemetry_data.get("VFR_HUD")
+        
+                    if (0 and ned and hud):
+
+                        # CALCULATE REAL LIFE DISTANCE
+                        detx, dety = img_det.get_distance((box[1] + box[3]) / 2,
+                        (box[2] + box[4]) / 2, 0, 0, hud.alt)
+
+                        c = sim.simulate(np.array((0, 0, hud.alt, ned.vx, ned.vy, ned.vz)), MDELAY)
+                        x, y, z = c[0:3]
+
+                        cls = box[0]
+
+                        if (abs(detx-x) < 2 and abs(dety-y) < 2 and firing == False):
+                            firing = True
+                            loggr.print("AUTO ACTIVATE MECHANISM", 3)
+                            p.ChangeDutyCycle(MAX_PWM if cls else MIN_PWM)
+                            firing = False
+
+            time.sleep(DET_WAIT)
+        
+        except Exception as e:
+            loggr.print("ERROR AT THREAD 0 " + str(e), 2)
+            time.sleep(ERROR_WAIT)
+
+
+## READ AND SEND IMG
+def read_send_img():
+
+    global img_feed
+
+    while True:
+        try:
+
+            if (img_recv.is_open and img_send.is_open):
+                img_feed = img_recv.recv()
+                if (img_feed is not None):
+                    print("sendimg")
+                    img_send.send(img_feed)
+                    read_check[3] += 1
+
+            time.sleep(IMG_WAIT)
+
+        except Exception as e:
+            loggr.print("ERROR AT THREAD 1 " + str(e), 2)
+            img_send.close()
+            img_recv.close()
+            time.sleep(ERROR_WAIT)
+            img_send.start()
+            img_recv.start()        
+
+
+
+## READ TELEMETRY FROM PIXHAWK AND DATA FROM GCS
+def read_data():
+    global telemetry_data, gcs_data
+
+    while True:
+        try:
+            readable, _, _ = select.select(
+                [mav_com.sock, mav_com.pixhawk.fd, mav_com.gcs_in.fd],
+                [], [], 0.01)
+
+            if (mav_com.pixhawk.fd in readable and mav_com.mav_connected):
+                msg = mav_com.read_pixhawk()        
+                if (msg):
+                    telemetry_data[msg.get_type()] = msg
+                    mav_com.send_planner(msg.get_msgbuf())
+                    read_check[0] += 1
+
+            if (mav_com.sock in readable and mav_com.sock_connected):
+                planner_data = mav_com.read_planner()
+                if (planner_data):
+                    mav_com.write_pixhawk(planner_data) 
+                    read_check[1] += 1
+
+            if (mav_com.gcs_in.fd in readable and mav_com.mav_connected):
+                msg = mav_com.get_gcs()
+                if (msg):
+                    if (gcs_data.get(msg.get_type()) == None):
+                        gcs_data[msg.get_type()] = [msg]
+                    else:
+                        gcs_data[msg.get_type()].append(msg)
+
+                    read_check[2] += 1
+
+            time.sleep(RECV_WAIT)
+        
+        except Exception as e:
+            loggr.print("ERROR AT THREAD 2 " + str(e), 2)
+            mav_com.close_sock()
+            mav_com.close_gcs()
+            time.sleep(ERROR_WAIT)
+            mav_com.connect_gcs(IP, *(PORTS[0:2]))
+            mav_com.connect_sock(MSIP, PORTS[2])        
+
+
+
+# SEND DATA TO GCS AND MISSION PLANNER
+def send_data():
+
+    global telemetry_data
+
+    while True:
+        try:
+
+            mav_com.send_heartbeat()
+
+            for _, msg in list(telemetry_data.items()):
+                    if (msg) and not msg.get_type().startswith("UNKNOWN_"):
+                        mav_com.send_gcs(msg)
+
+            # SEND BOXES
+            for box in box_data:
+                mav_com.send_box(box)
+
+            time.sleep(SEND_WAIT)
+
+        except Exception as e:
+            loggr.print("ERROR AT THREAD 3 " + str(e), 2)
+            time.sleep(ERROR_WAIT)  
+
+
+
+
+def log():
+
+    global read_check
+
+    while True:
+        if (signal_lost):
+            loggr.print("RC SIGNAL LOST!", 2)
+        else:
+            loggr.print("READ STATUS: ", 3, "")
+            loggr.raw_print("|", 0, "")
+            loggr.raw_print(f" PIXHAWK:{read_check[0]} ", 1 if read_check[0] else 2, "")
+            loggr.raw_print("|", 0, "")
+            loggr.raw_print(f" PLANNER:{read_check[1]} ", 1 if read_check[1] else 2, "")
+            loggr.raw_print("|", 0, "")
+            loggr.raw_print(f" GCS:{read_check[2]} " , 1 if read_check[2] else 2, "")
+            loggr.raw_print("|", 0, "")
+            loggr.raw_print(f" Camera:{read_check[3]} ", 1 if read_check[3] else 2, "")
+            loggr.raw_print("|", 0, "")
+            if telemetry_data.get("RC_CHANNELS"):
+                loggr.raw_print(f" ({telemetry_data.get('RC_CHANNELS').rssi}) ", 3)
+                loggr.raw_print("|", 0, "")
+
+        read_check = [0, 0, 0, 0]
+
+        time.sleep(LOG_WAIT)
+
+
+# PROCESS DATA
+def mainloop():
+
+    global gcs_data, is_det, firing, telemetry_data, signal_lost
+
+    lost_start = -1
+
+    while True:
+
+        """# FAILSAFE
+        if telemetry_data.get("RC_CHANNELS") and telemetry_data.get("RC_CHANNELS").rssi < SIGNAL_TRESHOLD:
+            if (lost_start >= 0 and time.time() - lost_start > FAILSAFE_DELAY):
+                #mav_com.send_fail()
+                loggr.print(" >>> FAIL SAFE <<< ", 2)            
+            else:
+                signal_lost = True
+                lost_start = time.time()
+        
+        elif signal_lost:
+            signal_lost = False
+            lost_start = -1"""
+
+
+        # PROCESS GCS DATA
+        blst = gcs_data.get("NAMED_VALUE_INT")
+        if (blst):
+            if (blst[-1].value == 0 and firing == False):
+                firing = True
+                loggr.print("ACTIVATE 1", 0)
+                p.ChangeDutyCycle(MIN_PWM) 
+                firing = False
+
+            elif (blst[-1].value == 1):
+                loggr.print("ARM TOGGLE", 0)
+                mav_com.toggle_arm(telemetry_data.get("HEARTBEAT"))
+
+            elif (blst[-1].value == 2):
+                is_det = False if is_det else True
+                loggr.print("DETECTION TOGGLE: " + is_det, 0)
+
+            elif (blst[-1].value == 3 and firing == False):
+                firing = True
+                loggr.print("ACTIVATE 2", 0)
+                p.ChangeDutyCycle(MAX_PWM)
+                firing = False
+
+            elif (blst[-1].value == 4):
+                loggr.print("TOGGLE CONTROL", 0)
+                mav_com.toggle_control(telemetry_data.get("HEARTBEAT"))
+
+            elif (blst[-1].value == 5 and firing == False):
+                firing = True
+                loggr.print("DE-ACTIVATE", 0)
+                p.ChangeDutyCycle(NET_PWM)
+                firing = False
+
+            blst.pop()
+
+        time.sleep(MAIN_WAIT)
+
+
+
+"""def save_img():
+    global img_feed
+
+    while True:
+        try:
+            if (img_feed is not None):
+                video.out.write(img_feed)
+
+            time.sleep(0.05)
+
+        except Exception as e:
+            loggr.print("ERROR AT SAVE 1 " + str(e), 2)"""   
+
+
+
+def write_telem():
+
+    global telemetry_data, telem_logger
+
+    telem_logger.set_start()
+
+    while True:
+        try:
+            telem_logger.write(telemetry_data)
+            time.sleep(0.1)
+
+        except Exception as e:
+            loggr.print("ERROR AT WRITE 1 " + str(e), 2)
+
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        MSIP = IP = sys.argv[1]
+
+    elif len(sys.argv) == 3:
+        IP = sys.argv[1]
+        MSIP = sys.argv[2]
+
+
+    loggr.print(f"Process Starting on GCS: {IP} - MSIP : {MSIP}...", 3, "\n\n")
+
+    loggr.print("Opening Camera Stream...", 3)
+    try:
+        if (img_recv.start()):
+            loggr.print("Success!\n", 1)
+        else:
+            loggr.print("Fail!\n", 2)
+    except:
+        loggr.print("Fail!\n", 2)
+
+
+    loggr.print("Starting Gstreamer...", 3)
+    try:
+        if (img_send.start()):
+            loggr.print("Success!\n", 1)
+        else:
+            loggr.print("Fail!\n", 2)
+    except:
+        loggr.print("Fail!\n", 2)
+
+    loggr.print("Waiting for Pixhawk Hearbeat...", 3)
+    pixhawk.wait_heartbeat()
+    loggr.print("Success!\n", 1)
+
+    loggr.print("Connecting GCS and Mission Planner...", 3)
+    mav_com.connect_gcs(IP, *(PORTS[0:2]))
+    mav_com.connect_sock(MSIP, PORTS[2])
+    loggr.print("Success!\n", 1)
+
+    loggr.print("Starting Servo GPIO...", 3)
+    p.start(NET_PWM)
+    loggr.print("Success!\n", 1)
+
+
+    #Thread(target=save_img, daemon=True).start()
+
+    #Thread(target=read_send_img, daemon=True).start()
+    #Thread(target=detect_and_fire, daemon=True).start()
+    Thread(target=send_data, daemon=True).start()
+    Thread(target=read_data, daemon=True).start()
+    Thread(target=log, daemon=True).start()
+    mainloop()
+
+    p.stop()
+    GPIO.cleanup()
---
+++ rpi_code/util/telem_log.py
@@ -0,0 +1,75 @@
+
+import time, datetime
+
+class TelemLog:
+
+	def __init__(self, name):
+		self.log_file = open(name, "w")
+		self.start_time = 0
+
+	def get_time(self):
+		return datetime.datetime.now().strftime("%H:%M:%S") + f".{datetime.datetime.now().microsecond // 1000:03d}"
+
+	def set_start(self):
+		self.log_file.write(self.get_time() + "\n")
+
+	def write(self, telem_dict):
+
+		roll = "Nan"
+		pitch = "Nan"
+		yaw = "Nan"
+		airspeed = "Nan"
+		groundspeed = "Nan"
+		climb = "Nan"
+		heading = "Nan"
+		alt = "Nan"
+		lat = "Nan"
+		lon = "Nan"
+		battery_volt = "Nan"
+		battery_per = "Nan"
+		throttle = "Nan"
+		ch1 = "Nan"
+		ch2 = "Nan"
+		ch3 = "Nan"
+
+		attitude = telem_dict.get("ATTITUDE")
+		vfr_hud = telem_dict.get("VFR_HUD")
+		gps = telem_dict.get("GLOBAL_POSITION_INT")
+		sys_stat = telem_dict.get("SYS_STATUS")
+		rc_channel = telem_dict.get("RC_CHANNELS")
+
+		# Attitude
+		if (attitude):
+			roll = (attitude.roll / 2*np.pi)*360
+			pitch = (attitude.pitch / 2*np.pi)*360
+			yaw = (attitude.yaw / 2*np.pi)*360
+
+		# Airspeed, Ground speed, Altitude, Heading
+		if (vfr_hud):
+			airspeed = vfr_hud.airspeed
+			groundspeed = vfr_hud.groundspeed
+			climb = vfr_hud.climb
+			heading = vfr_hud.heading
+			alt = vfr_hud.alt
+
+		# GPS Position
+		if (gps):
+			lat = msg.lat / 1e7
+			lon = msg.lon / 1e7
+    
+		# Battery Status
+		if (sys_stat):
+		    battery_volt = msg.voltage_battery / 1000.0
+		    battery_per = msg.battery_remaining
+    
+		# Control Inputs (throttle, roll, pitch, yaw)
+		if (rc_channel):
+		    cont_inputs = [msg.chan3_raw, msg.chan1_raw, msg.chan2_raw, msg.chan4_raw]
+		    throttle, ch1, ch2, ch3 = tuple([(max(0, min(1, ((x-988) / 993))) if x is not None else 0) for x in self.cont_inputs])
+
+		self.log_file.write( self.get_time() + ", " + 
+			", ".join(["{:.2f}".format(x) for x in (roll, pitch, yaw, airspeed, groundspeed, climb, heading,
+			 alt, lat, lon, battery_volt, battery_per, throttle, ch1, ch2, ch3)]) + "\n")
+
+	def close(self):
+		self.log_file.close()
