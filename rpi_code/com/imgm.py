-image distance calculation fix

rpi_code/com/imgm.py
@@ -14,8 +14,6 @@ class DetectClass:
 		self.fx, self.fy, self.cx, self.cy = fx, fy, cx, cy
 		self.default_pitch = default_pitch
 
-		print("Model is ready...")
-
 	def get_boxes(self, img):
 		return self.model.predict(img, show = False)[0].boxes
 
@@ -25,8 +23,11 @@ class DetectClass:
 		Y = (y - self.cy) / self.fy
 		
 		r_cam = np.array([X, Y, 1.0])
-		r = Rotation.from_euler('xyz', [roll, pitch + self.default_pitch, 0], degrees=True)
-		r_world = r.inv().apply(r_cam)
+		r = Rotation.from_euler('xyz', [pitch + self.default_pitch, roll, 0], degrees=False)
+		r_world = r.apply(r_cam)
+
+		if r_world[2] <= 0:
+			raise ValueError("horizon error")
 
 		t = h / r_world[2]
 

rpi_code/main.py
@@ -83,12 +83,15 @@ def detect_and_fire():
                     # RUN SIMULATION
                     ned = telemetry_data.get("LOCAL_POSITION_NED")
                     hud = telemetry_data.get("VFR_HUD")
+                    attd = telemetry_data.get("ATTITUDE")
         
-                    if (ned and hud):
-
-                        # CALCULATE REAL LIFE DISTANCE
-                        detx, dety = img_det.get_distance((box[1] + box[3]) / 2,
-                        (box[2] + box[4]) / 2, 0, 0, hud.alt)
+                    if (ned and hud and attd):
+                        try:
+                            # CALCULATE REAL LIFE DISTANCE
+                            detx, dety = img_det.get_distance((box[1] + box[3]) / 2,
+                            (box[2] + box[4]) / 2, attd.roll, attd.pitch, hud.alt)
+                        except ValueError:
+                            continue
 
                         c = sim.simulate(np.array((0, 0, hud.alt, ned.vx, ned.vy, ned.vz)), MDELAY)
                         x, y, z = c[0:3]
@@ -438,7 +441,7 @@ try:
     
         ################## IMAGE CLASSES ##################
         loggr.print("Starting Detection Model...", 3)
-        img_det = DetectClass("model.pt", 1071, 1071, 320, 240, 0)
+        img_det = DetectClass("model.pt", 1071, 1071, 320, 240, np.pi / 4)
         loggr.print("Success!\n", 1)
     
         loggr.print("Starting Camera Reader Class...", 3)