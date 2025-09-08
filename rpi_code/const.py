6569a4521086c29ffe2b3fa1c926644f12bb6258
Tuna Ekici <tnaekc2@gmail.com>
2025-09-08 09:51:27 +0300

    -const update

--- rpi_code/const.py
+++ rpi_code/const.py
@@ -1,19 +1,22 @@
 
 ############### DELAY CONST ################
 
-DET_WAIT = 0.03
+DET_WAIT = 0.05
 MAIN_WAIT = 0.1
-SEND_WAIT = 0.1
-IMG_WAIT = 0.01
+SEND_WAIT = 0.05
+IMG_WAIT = 0.02
 RECV_WAIT = 0
 LOG_WAIT = 1
+
+PWMRET_WAIT = 3
+PWM_WAIT = 2
 
 ERROR_WAIT = 0.1
 
 #############################################
 
-FAILSAFE_DELAY = 1
-FAILSAFE_ACTIVE = False
+FAILSAFE_DELAY = 0.5
+FAILSAFE_ACTIVE = True
 
 ############## NETWORK CONST ###############
 
@@ -26,15 +29,15 @@ PORTS = (14550, 14551, 31313) # send recv mp
 
 ################ SERVO CONST ################
 
-MIN_PWM = 2
-NET_PWM = 8
-MAX_PWM = 13
+MIN_PWM = 0.4
+NET_PWM = 9
+MAX_PWM = 15
 MDELAY = 1
 SERVO_PIN = 12
 
 ################ DETECTION CONST ################
 
-REQUIRED_DETECTION_COUNT = 1
+REQUIRED_DETECTION_COUNT = 2
 DETECTION_TIMEOUT = 3
 
 MAX_SHOOT_DIST = 5
@@ -43,12 +46,14 @@ MAX_DIST = 5
 SHOOT_COOLDOWN = 5
 
 SEARCH_TIMEOUT = 5
-DET_CONF = 0.6
+DET_CONF = 0.7
 
 IS_MANUAL = True
-SIMPLE_FIRE = False
+SIMPLE_FIRE = True
 
-STRETCH_FIX = 0.7
+IMG_FPS = 30
+IMG_WIDTH = 640
+IMG_HEIGHT = 480
 
 ############################################
 
@@ -64,3 +69,5 @@ ERROR_TRY_COUNT = 3
 PC_TEST = False
 IMG_TEST = True
 TELEM_TEST = False
+
+USE_GCS_MSN = True
